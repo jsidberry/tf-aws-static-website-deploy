7fcac9028268b75791a895ee128b763b9bb4bc22
Juan Sidberry <jsidberry@gmail.com>
2021-03-06 21:06:22 -0500

    version values must be static literals

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
-  required_version = var.tf_required_version
+  required_version = ">=0.14.0"
   required_providers {
-    aws = var.aws_required_version
+    aws = ">=3.0.0"
   }
   backend "s3" {
     region  = var.aws_region
