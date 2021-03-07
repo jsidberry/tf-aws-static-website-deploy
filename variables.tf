a3bd453ef98319af606a97691a0331add51de203
Juan Sidberry <jsidberry@gmail.com>
2021-03-06 19:30:12 -0500

    add variables

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "avail_zone" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "aws_required_version" {}
+variable "env_prefix" {}
+variable "image_name" {}
+variable "instance_type" {}
+variable "my_ip" {}
+variable "public_key_location" {}
+variable "subnet_cidr_block" {}
+variable "tf_required_version" {}
+variable "tf_statefile_bucket" {}
+variable "tf_statefile_key" {}
+variable "vpc_cidr_block" {}
+variable "website-domain-main" {}
+variable "website-domain-redirect" {}
+# variable "environment" {}
