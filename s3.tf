2c72635175fe94bc585e1862b4883132b63025fc
Juan Sidberry <jsidberry@gmail.com>
2021-03-06 20:11:49 -0500

    add files storage with s3

---
+++ s3.tf
@@ -0,0 +1,71 @@
+## S3
+# Creates bucket to store logs
+resource "aws_s3_bucket" "website_logs" {
+  bucket = "${var.website-domain-main}-logs"
+  acl    = "log-delivery-write"
+
+  # Comment the following line if you are uncomfortable with Terraform destroying the bucket even if this one is not empty 
+  force_destroy = true
+
+  tags = {
+    ManagedBy = "terraform"
+    Changed   = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+# Creates bucket to store the static website
+resource "aws_s3_bucket" "website_root" {
+  bucket = "${var.website-domain-main}-root"
+  acl    = "public-read"
+
+  # Comment the following line if you are uncomfortable with Terraform destroying the bucket even if not empty 
+  force_destroy = true
+
+  logging {
+    target_bucket = aws_s3_bucket.website_logs.bucket
+    target_prefix = "${var.website-domain-main}/"
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+
+  tags = {
+    ManagedBy = "terraform"
+    Changed   = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+# Creates bucket for the website handling the redirection (if required), e.g. from https://www.example.com to https://example.com
+resource "aws_s3_bucket" "website_redirect" {
+  bucket        = "${var.website-domain-main}-redirect"
+  acl           = "public-read"
+  force_destroy = true
+
+  logging {
+    target_bucket = aws_s3_bucket.website_logs.bucket
+    target_prefix = "${var.website-domain-main}-redirect/"
+  }
+
+  website {
+    redirect_all_requests_to = "https://${var.website-domain-main}"
+  }
+
+  tags = {
+    ManagedBy = "terraform"
+    Changed   = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
