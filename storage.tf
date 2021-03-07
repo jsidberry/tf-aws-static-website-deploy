6b835a90bce919fda391646360815ec1495c131b
Juan Sidberry <jsidberry@gmail.com>
2021-03-06 20:24:12 -0500

    add storage policies

--- storage.tf
+++ storage.tf
@@ -69,3 +69,30 @@ resource "aws_s3_bucket" "website_redirect" {
     ignore_changes = [tags]
   }
 }
+
+# Creates policy to allow public access to the S3 bucket
+resource "aws_s3_bucket_policy" "update_website_root_bucket_policy" {
+  bucket = aws_s3_bucket.website_root.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "PolicyForWebsiteEndpointsPublicContent",
+  "Statement": [
+    {
+      "Sid": "PublicRead",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.website_root.arn}/*",
+        "${aws_s3_bucket.website_root.arn}"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
