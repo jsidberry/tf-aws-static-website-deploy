476130e674a43e4eeca85d3aeeeb751795048378
Juan Sidberry <jsidberry@gmail.com>
2021-03-06 22:09:56 -0500

    rm unnecessary variables

--- variables.tf
+++ variables.tf
@@ -1,18 +1,14 @@
-variable "avail_zone" {}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
-# variable "aws_required_version" {}
-variable "env_prefix" {}
-variable "image_name" {}
-variable "instance_type" {}
-variable "my_ip" {}
-variable "public_key_location" {}
-variable "subnet_cidr_block" {}
-# variable "tf_required_version" {}
-# variable "tf_statefile_bucket" {}
-# variable "tf_statefile_key" {}
-variable "vpc_cidr_block" {}
-variable "website-domain-main" {}
-variable "website-domain-redirect" {}
-# variable "environment" {}
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "website-domain-main" {
+    type = string
+    default = "juansidberry.com"
+}
+
+variable "website-domain-redirect" {
+    type = string
+    default = "www.juansidberry.com"
+}
