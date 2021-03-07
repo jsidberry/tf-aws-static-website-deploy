faf23dd2767e4570a5483c5dae361f9ae7bda5d6
Juan Sidberry <jsidberry@gmail.com>
2021-03-06 22:11:19 -0500

    format files

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
-    type = string
-    default = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "website-domain-main" {
-    type = string
-    default = "juansidberry.com"
+  type    = string
+  default = "juansidberry.com"
 }
 
 variable "website-domain-redirect" {
-    type = string
-    default = "www.juansidberry.com"
+  type    = string
+  default = "www.juansidberry.com"
 }
