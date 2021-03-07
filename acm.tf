85231b62bfdfd51e2abb592519b1928f4d6e3f98
Juan Sidberry <jsidberry@gmail.com>
2021-03-06 20:06:20 -0500

    add certificate validation

---
+++ acm.tf
@@ -0,0 +1,42 @@
+## ACM (AWS Certificate Manager)
+# Creates the wildcard certificate *.<yourdomain.com>
+# Wilcard certificate used by CloudFront requires this specific region (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/cnames-and-https-requirements.html)
+resource "aws_acm_certificate" "wildcard_website" {
+  provider = aws.us-east-1 
+
+  domain_name               = var.website-domain-main
+  subject_alternative_names = ["*.${var.website-domain-main}"]
+  validation_method         = "DNS"
+
+  tags = {
+    ManagedBy = "terraform"
+    Changed   = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+# Triggers the ACM wildcard certificate validation event
+resource "aws_acm_certificate_validation" "wildcard_cert" {
+  provider = aws.us-east-1
+
+  certificate_arn         = aws_acm_certificate.wildcard_website.arn
+  validation_record_fqdns = [aws_route53_record.wildcard_validation.fqdn]
+}
+
+# Get the ARN of the issued certificate
+data "aws_acm_certificate" "wildcard_website" {
+  provider = aws.us-east-1
+
+  depends_on = [
+    aws_acm_certificate.wildcard_website,
+    aws_route53_record.wildcard_validation,
+    aws_acm_certificate_validation.wildcard_cert,
+  ]
+
+  domain      = var.website-domain-main
+  statuses    = ["ISSUED"]
+  most_recent = true
+}
